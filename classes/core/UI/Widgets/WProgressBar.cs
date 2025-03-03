b74a28ed77754693304434b549244745f7294af8
Jemko <jemko23@t-online.de>
2025-03-03 13:15:44 +0100

    Update WProgressBar.cs

--- classes/core/UI/Widgets/WProgressBar.cs
+++ classes/core/UI/Widgets/WProgressBar.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Tiled.UI.Widgets
 {
-    public class WProgressBar
+    public class WProgressBar : Widget
     {
+        public Texture2D backgroundTexture;
+        public Texture2D fillTexture;
+
+        public Color backgroundColor;
+        public Color fillColor;
+
+        public WProgressBar(HUD owner) : base(owner)
+        {
+        }
+
+        public override void Construct()
+        {
+            backgroundTexture = new Texture2D(Program.GetGame().GraphicsDevice, 1, 1);
+            fillTexture = new Texture2D(Program.GetGame().GraphicsDevice, 1, 1);
+        }
+
+        public override void DrawWidget(ref SpriteBatch sb)
+        {
+            sb.Draw(backgroundTexture, scaledGeometry, backgroundColor);
+            sb.Draw(fillTexture, scaledGeometry, fillColor);
+        }
     }
 }
